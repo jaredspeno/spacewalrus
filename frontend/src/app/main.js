d341933a132c2cf69bffeb9a012127875217e5f7
Gregory Shayko <greg@shaykos.com>
2017-07-08 15:41:14 -0400

    Added basic question layout

--- frontend/src/app/main.js
+++ frontend/src/app/main.js
@@ -1,3 +1,14 @@
 export const main = {
-  template: require('./main.html')
+  template: require('./main.html'),
+  controller() {
+    this.question = {
+      questionId: 42,
+      title: 'I am a question TITLE!',
+      content: 'WOOOOOHOOO!',
+      userId: 'SHAYKOG',
+      tags: [
+        'java', 'woot'
+      ]
+    };
+  }
 };
