7a9f85a5b5f5fcdc1524b48cf5d8ddba53b584c3
coatsnmore <coats.18@gmail.com>
2017-07-08 14:31:01 -0400

    added upate, put, to questions entity

--- src/main/java/com/spacewalrus/Questions.java
+++ src/main/java/com/spacewalrus/Questions.java
@@ -1,11 +1,11 @@
 package com.spacewalrus;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.spacewalrus.entities.Question;
-import com.spacewalrus.entities.Tag;
 import com.spacewalrus.repository.QuestionRepository;
 
 @Controller
@@ -24,24 +23,21 @@ public class Questions {
 
 	@Autowired
 	private QuestionRepository questionRepository;
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Question> getAllQuestions() {
-//		List<Question> questions = defaultData();
-
 		List<Question> questions = (List<Question>) questionRepository.findAll();
-		
 		return questions;
 	}
-	
+
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	@POST
 	public Question createQuestion(Question question) {
-		
+
 		questionRepository.save(question);
-		
+
 		return question;
 	}
 
@@ -50,69 +46,23 @@ public class Questions {
 	@Path("/{id}")
 	public Question getQuestionById(@PathParam("id") Long id) {
 		
-//		Question question = findByIdHardcoded(id);
 		Question question = (Question) questionRepository.findOne(id);
-
-		return question;
-	}
-
-	private Question findByIdHardcoded(Long id) {
-		System.out.println("finding quesiton by id: " + id);
 		
-		Question question = new Question();
-
-		List<Tag> tags = new ArrayList<Tag>();
-		Tag tag1 = new Tag();
-		tag1.setTag("stupid-quesiton");
-		tags.add(tag1);
-
-		if (id == 1) {
-			Question question1 = new Question();
-			question1.setTitle("stupid fucking question");
-			question1.setContent("like umm.. here's my stupid quesiton or whatever");
-			question1.setQuestionId(1L);
-			question1.setUserId("userX");
-//			question1.setTags(tags);
-			question = question1;
-		} else if (id == 2) {
-			Question question2 = new Question();
-			question2.setTitle("great question");
-			question2.setContent("like umm.. here's my great quesiton or whatever");
-			question2.setQuestionId(2L);
-			question2.setUserId("userY");
-//			question2.setTags(tags);
-			question = question2;
-		} else {
-			question = new Question();
-		}
 		return question;
 	}
 
-	private ArrayList<Question> defaultData() {
-		List<Tag> tags = new ArrayList<Tag>();
-		Tag tag1 = new Tag();
-		tag1.setTag("stupid-quesiton");
-		tags.add(tag1);
-
-		ArrayList<Question> questions = new ArrayList<Question>();
-
-		Question question1 = new Question();
-		question1.setTitle("stupid fucking question");
-		question1.setContent("like umm.. here's my stupid quesiton or whatever");
-		question1.setQuestionId(1L);
-		question1.setUserId("userX");
-//		question1.setTags(tags);
-
-		Question question2 = new Question();
-		question2.setTitle("great question");
-		question2.setContent("like umm.. here's my great quesiton or whatever");
-		question2.setQuestionId(2L);
-		question2.setUserId("userY");
-//		question2.setTags(tags);
-
-		questions.add(question1);
-		questions.add(question2);
-		return questions;
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public Question updateQuestionById(@PathParam("id") Long id, Question question) {
+		
+		Question existingQuestion = (Question) questionRepository.findOne(id);
+		existingQuestion.setContent(question.getContent());
+		existingQuestion.setTitle(question.getTitle());
+		existingQuestion.setUserId(question.getUserId());
+		questionRepository.save(existingQuestion);
+		
+		return question;
 	}
-
 }
